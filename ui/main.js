cf44787a04bbe57ea31575d08233c4b1c8d44cd7
Sourabh Supnekar <30634944+M4gnum@users.noreply.github.com>
2017-08-14 18:45:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var element = document.getElementById('new1');
 element.innerHTML = 'New home page';
 
 var imgElement = document.getElementById('img1');
-imgElement.onClick = function() {
+imgElement.onclick = function() {
     imgElement.style.marginLeft = '100px';
 };
