2e412ed1a812ae31a72a5c20df32df5e58a6e0d8
Sourabh Supnekar <30634944+M4gnum@users.noreply.github.com>
2017-08-14 18:53:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,11 @@ var element = document.getElementById('new1');
 element.innerHTML = 'New home page';
 
 var imgElement = document.getElementById('img1');
-var moveVal = 0;
+var marginLeft = 0;
 function moveRight()
 {
-    moveVal = moveVal + 10;
-    imgElement.style.marginLeft = moveval + 'px';
+    marginLeft = marginLeft + 10;
+    imgElement.style.marginLeft = marginLeft + 'px';
 }
 imgElement.onclick = function() {
     var interval = setInterval(moveRight, 100);
