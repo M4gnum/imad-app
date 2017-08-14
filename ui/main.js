ac4bf5aab45ff5e0049f20f4ad6c52148d3b8421
Sourabh Supnekar <30634944+M4gnum@users.noreply.github.com>
2017-08-14 18:54:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,9 +7,9 @@ var imgElement = document.getElementById('img1');
 var marginLeft = 0;
 function moveRight()
 {
-    marginLeft = marginLeft + 1;
+    marginLeft = marginLeft + 2;
     imgElement.style.marginLeft = marginLeft + 'px';
 }
 imgElement.onclick = function() {
-    var interval = setInterval(moveRight, 10);
+    var interval = setInterval(moveRight, 20);
 };
