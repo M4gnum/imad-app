ce1d8d8bb389b9685fa75ef6ae8d8636ea9ec15d
Sourabh Supnekar <30634944+M4gnum@users.noreply.github.com>
2017-08-14 18:42:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('new1');
 element.innerHTML = 'New home page';
+
+var imgElement = document.getElementById('img1');
+imgElement.onClick = function() {
+    imgElement.style.margin = '100px';
+};
