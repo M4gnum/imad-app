03934d130ff61e085be31432b5700625a6b9657a
Sourabh Supnekar <30634944+M4gnum@users.noreply.github.com>
2017-08-15 01:56:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,21 +23,34 @@ button.onclick = function(){
 };
 
 //Submit name
-var inputName = document.getElementById('name');
-var Name = inputName.value;
+
 var submit = document.getElementById('b1');
 
 submit.onclick = function(){
     //get request
+    var request = new XMLHttpRequest();
     
-    //render names
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                //render names
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i = 0;i < names.length; i++)
+                {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('ulist');
+                ulist.innerHTML = list;
+            }
+        }
+    };
     
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for(var i = 0;i < names.length; i++)
-    {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('ulist');
-    ulist.innerHTML = list;
+    var inputName = document.getElementById('name');
+    var Name = inputName.value;
+    request.open('GET', 'http://sourabhsupnekar20.imad.hasura-app.io/submit-name?name=' + Name, true);
+    request.send(null);
 };
