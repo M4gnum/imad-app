f18ca270624f7895e1aaf8f688ff1258e914ec55
Sourabh Supnekar <30634944+M4gnum@users.noreply.github.com>
2017-08-29 00:00:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/hash/:input', function(req, res){
 });
 
 app.post('/create-user', function(req,res){
-    //username. password
+    //username, password
     var username = req.body.username;
     var password = req.body.password;
     
