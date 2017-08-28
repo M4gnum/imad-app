52aa26f4e66bfc8f4d4b7a45e31de8acf07711ad
Sourabh Supnekar <30634944+M4gnum@users.noreply.github.com>
2017-08-29 00:52:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,6 +31,6 @@ submit.onclick = function(){
     console.log(username);
     console.log(password);
     request.open('POST', 'http://sourabhsupnekar20.imad.hasura-app.io/login', true);
-    request.setRequestHeader('Content-Type', 'application/json')
+    request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username : username, password: password}));
 };
