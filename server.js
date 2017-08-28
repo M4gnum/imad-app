79f8353e7deda93f22d9ac046413edac771968fc
Sourabh Supnekar <30634944+M4gnum@users.noreply.github.com>
2017-08-29 01:08:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'sourabhsupnekar20',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'randomVal',
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30 }
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -138,6 +143,8 @@ app.post('/login', function(req, res){
                 var hashedPassword = hash(password, salt);
                 if(hashedPassword === dbString)
                 {
+                    req.session.auth = {userId: result.rows[0].id};
+                    
                     res.send('Credentials are correct');
                 }
                 else
@@ -147,6 +154,17 @@ app.post('/login', function(req, res){
             }
         }
    });
+});
+
+app.get('/check-login', function(req, res){
+   if(req.session && req.session.auth && req.session.auth.userId) 
+   {
+       res.send('Successfully logged in :' + req.session.auth.userId.toString());
+   }
+   else
+   {
+       res.send('Not loggged in');
+   }
 });
 
 app.get('/articles/:articleName', function(req, res){
